b026f1ad71203c6ee16d61cfaf66696f8e5bc5e5
Andrei Dubau <Andrei Dubau>
2016-10-18 21:04:50 +0300

    changes to styling

--- public/app/app.js
+++ public/app/app.js
@@ -13,3 +13,24 @@ angular
     'vsGoogleAutocomplete'
   ]);
 })();
+
+$(document).ready(function() {
+  $(document).on('click', 'button', function() {
+    // $.ajax('/photos.html',{
+    //   success: function(data){
+    //   	$(".photos").html(data);
+    //
+    //   }
+    // });
+        $.ajax({
+            url: 'app/user.json',
+            type: 'get',
+            dataType: 'json',
+            success: function(data){
+                var userInfo = data;
+                $('.user').append('<h5>' + userInfo.employees[0].firstName + '</h5>').show('slow');
+                console.log(data);
+            }
+        });
+    });
+});
