a5dd14871989f05d2e723e9c97b8248b4f421234
Andrei Dubau <Andrei Dubau>
2016-12-05 20:58:50 +0200

    Added contacts app

--- public/app/controllers/contactsCtrl.js
+++ public/app/controllers/contactsCtrl.js
@@ -8,13 +8,48 @@
   ContactsCtrl.$inject = ['$scope', '$http'];
 
   function ContactsCtrl($scope, $http) {
-    $.ajax({
-        url: "https://contactos-afbe1.firebaseio.com/",
-        type:"GET",
-        dataType: "json",
-        success: function(res) {
-          console.log(res);
-        }
-    })
+
+
+       $.ajax({
+         url: 'https://contactos-afbe1.firebaseio.com/.json',
+         type: "GET",
+
+         success: function (res) {
+             console.log("first call", res);
+             $.each(res.users, function (index, value) {
+                 //console.log("success", index);
+
+                 //in value vin obiectele din backend
+                 console.log("success", value);
+
+                 //exemplu de cum afisam info in body
+                 $("body").append("<span>" + value.name + " </span>");
+             });
+         },
+
+         error: function(error) {
+           alert("error: "+error);
+         }
+
+       });
+       //
+    //       var newUser =  {
+    //           id: 3,
+    //           name: "Razvan Dubau",
+    //           phone: "inca nu am numar de tel",
+    //           email: "are mami si tati"
+    //       };
+       //
+    //       $.ajax({
+    //         url: 'https://contactos-afbe1.firebaseio.com/users.json',
+    //         type: "POST",
+    //         data: JSON.stringify(newUser),
+    //         success: function (res) {
+    //           console.log(res);
+    //         },
+    //         error: function(error) {
+    //           alert("error: "+error);
+    //         }
+    //    });
   }
 }());
